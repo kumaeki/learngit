726734b1919a88d63a18b5bbfdcd0c463bc60051
kumaeki <tedyixiong@gmail.com>
2018-12-07 17:33:24 +0900

    add python file 2018/12/07

--- do_multiprosessing_subprocess.py
+++ do_multiprosessing_subprocess.py
@@ -1,3 +1,12 @@
 import subprocess
 
 print("$ nslookup www.python.org")
+#r=subprocess.call(["nslookup","www.python.org"])
+p = subprocess.Popen(["nslookup"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output,err=p.communicate(b"set q=mx\npython.org\nexit\n")
+#output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+print(output.decode("shift-jis"))
+#print(output.decode('utf-8'))
+print("Exit code:",p.returncode)
+#print('Exit code:', p.returncode)
